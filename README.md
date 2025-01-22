b7aec33080f947067d5b4a8090d0911fca061309
DayronTorresYegua <dtorresy@iesrafaelalberti.es>
2025-01-22 11:51:20 +0100

    Implementación de la Funcionalidad

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # presenciaIpEnRed
+- El propósito de esta práctica es desarrollar un módulo en Python que reciba como entrada una dirección IP y determine si está activa en la red. Para lograrlo, el módulo deberá realizar un "ping" o un proceso similar a la dirección proporcionada y devolver si la dirección responde o no.
+
+
+1. Validación de Datos de Entrada:
+Es fundamental que el módulo compruebe que la dirección IP introducida es válida antes de intentar realizar cualquier acción. Deberás implementar una verificación adecuada que garantice que solo se procesen direcciones IP válidas.
+
+2. Implementación de la Funcionalidad:
+
+El módulo debe ser capaz de identificar el sistema operativo desde el que se ejecuta (Windows, Linux o macOS)
+
+3. Salida del Módulo:
+
+Si la dirección IP responde, el módulo debe indicar claramente que está activa.
+Si no responde, debe notificar que la dirección IP no está activa o no es accesible.
+Debe notificar al usuario el sistema operativo en el cual esta corriendo el modulo.
+
+4. Originalidad del Código:
+
+No se permitirá el uso de herramientas generadas por inteligencia artificial.
+Cada alumno deberá desarrollar una solución única, demostrando su creatividad y capacidad de razonamiento lógico
---
+++ presencia.py
@@ -0,0 +1,53 @@
+import os
+import sys
+
+# 1. Validación de Datos de Entrada:
+
+def validarIp(ip):
+    octetos = ip.split('.')
+    if len(octetos) != 4:
+        return False
+    for octeto in octetos:
+        if not octeto.isdigit():
+            return False
+        if int(octeto) < 0 or int(octeto) > 255:
+            return False
+    return True
+
+def determinarClase(ip):
+    primerOcteto = int(ip.split('.')[0])
+    if 0 <= primerOcteto <= 127:
+        return 'A'
+    elif 128 <= primerOcteto <= 191:
+        return 'B'
+    elif 192 <= primerOcteto <= 223:
+        return 'C'
+    elif 224 <= primerOcteto <= 239:
+        return 'D'
+    elif 240 <= primerOcteto <= 255:
+        return 'E'
+
+# 2. Implementación de la Funcionalidad:
+
+def nombreSistemaOperativo():
+    nombreSistema = os.uname().sysname, os.uname().version
+    return nombreSistema
+
+def main():
+    if len(sys.argv) != 2:
+        print("La ip no debe contener espacios en sus octetos")
+        sys.exit(1)
+
+    direccionIp = sys.argv[1]
+
+    if validarIp(direccionIp):
+        clase = determinarClase(direccionIp)
+        print(f"La dirección IP {direccionIp} es de clase {clase}.")
+    else:
+        print("La dirección IP ingresada no es válida, debe contener 4 octetos con numeros comprendidos entre 0 y 255.")
+
+    sistemaOperativo = nombreSistemaOperativo()
+    print(f"El sistema operativo es: {sistemaOperativo}")
+
+if __name__ == "__main__":
+    main()
